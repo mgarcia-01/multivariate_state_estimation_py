bd5be120745e1a43d660d4505dafb9a0fd0509d4
Michael Garcia <mgarcia4703@outlook.com>
2026-08-05 17:55:46 -0400

    fixed synthetic data removed index col

--- src/synthethic_data.py
+++ src/synthethic_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sdv.single_table import GaussianCopulaSynthesizer
 from sdv.metadata import Metadata
 
-data = pd.read_csv('data/options_data.csv')
+data = pd.read_csv('data/test.csv')
 metadata = Metadata.detect_from_dataframe(data)
 
 synthesizer = GaussianCopulaSynthesizer(metadata)
@@ -11,5 +11,6 @@ synthetic_data = synthesizer.sample(num_rows=1000)
 #metadata.save_to_json('metadata.json')
 
 # in the future, you can reload the metadata object from the file
-metadata = Metadata.load_from_json('metadata.json')
-synthetic_data.to_csv("data/syn_options.csv")
+#metadata = Metadata.load_from_json('metadata.json')
+#synthetic_data.to_csv("data/syn_test.csv")
+synthetic_data.to_csv("data/syn_train.csv", index = False)
