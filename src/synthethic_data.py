4f7b6f368d6b8575e20a4e3a05e8d05b35a3a305
Michael Garcia <mgarcia4703@outlook.com>
2026-08-07 17:34:11 -0400

    updated syntehtic data

--- src/synthethic_data.py
+++ src/synthethic_data.py
@@ -2,15 +2,21 @@ import pandas as pd
 from sdv.single_table import GaussianCopulaSynthesizer
 from sdv.metadata import Metadata
 
-data = pd.read_csv('data/test.csv')
+model_data = "test"
+file = 'data/'+model_data+'.csv'
+data = pd.read_csv(file)
+#data = pd.read_csv('data/training.csv')
+
 metadata = Metadata.detect_from_dataframe(data)
 
 synthesizer = GaussianCopulaSynthesizer(metadata)
 synthesizer.fit(data)
-synthetic_data = synthesizer.sample(num_rows=1000)
+synthetic_data = synthesizer.sample(num_rows=100)
 #metadata.save_to_json('metadata.json')
 
 # in the future, you can reload the metadata object from the file
 #metadata = Metadata.load_from_json('metadata.json')
 #synthetic_data.to_csv("data/syn_test.csv")
-synthetic_data.to_csv("data/syn_train.csv", index = False)
+out_data = 'data/syn_'+model_data+'.csv'
+synthetic_data.to_csv(out_data, index = False)
+#synthetic_data.to_csv("data/syn_train.csv", index = False)
